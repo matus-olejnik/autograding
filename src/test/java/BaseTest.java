efded06a2b299c7b32afeedb0b37b5a73b4c35bd
Matus Olejnik <matus.olejnik@gmail.com>
2021-12-09 12:21:21 +0100

    feat: add tests

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,6 +5,18 @@ public class BaseTest {
     @Test
     public void testFirst2b() {
 
-        System.out.println("TEST 1 (2b)");
+        System.out.println("TEST 1 (2b) - CORRECT");
+    }
+
+    @Test
+    public void testSecond3b() {
+
+        System.out.println("TEST 2 (3b) - FAIL");
+    }
+
+    @Test
+    public void testThird1b() {
+
+        System.out.println("TEST 3 (1b) - CORRECT");
     }
 }
