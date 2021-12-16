5ba25fdabf4a061371fc67ee5b29bc140f95c407
Matus Olejnik <matus.olejnik@gmail.com>
2021-12-16 15:28:29 +0100

    feat: testing GitHub action trigger

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -2,6 +2,5 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Starting...");
         System.out.println("Starting...");
-        System.out.println("Starting...");
     }
 }
