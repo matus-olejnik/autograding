4692e0d38c36ec1f3236044f8a9f48361f906ecf
Matus Olejnik <matus.olejnik@gmail.com>
2022-04-26 09:58:08 +0200

    feat: testing GitHub action

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,5 +3,6 @@ public class MainClass {
     public static void main(String[] args) {
 //        TODO implementation
         System.out.println("Test");
+        System.out.println("Test");
     }
 }
